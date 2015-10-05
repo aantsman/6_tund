9d451bf81b6c87221108d75b847510a10b8d16f2
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-10-05 15:12:51 +0300

    hidden input

--- table.php
+++ table.php
@@ -43,6 +43,9 @@
 		if(isset($_GET["edit"]) && $_GET["edit"]==$car_array[$i]->id){
 			echo "<tr>";
 			echo "<form action='table.php' method='get'>";
+			
+			//input mida välja ei näidata 
+			echo "<input type='hidden' name='car_id' value='".$car_array[$i]->id."'>";
 			echo "<td>".$car_array[$i]->id."</td>";
 			echo "<td>".$car_array[$i]->user_id."</td>";
 			echo "<td><input name='number_plate' value=".$car_array[$i]->number_plate."></td>";
